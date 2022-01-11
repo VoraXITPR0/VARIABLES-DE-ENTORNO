06bff79879078c1b9faee9dd1bdb56638ef70a24
VoraXITPR0 <a2107507@institutmontilivi.cat>
2022-01-11 10:22:03 +0000

    PRACTICA VARIABLES ENTORNO POST EXPLICACION MARC

---
+++ SCRIPT_TXT_HASH.py
@@ -0,0 +1,46 @@
+########  SCRIPT LLEGIR ARXIU TXT i crear HASH MD5,SHA1,SHA256,SHA512 iSHA3-512 By Andrés Manso  #############################
+
+from io import open  # IMPORTO modul IO i metode "OPEN" ( permet obrir un arxiu extern)
+import hashlib       # IMPORTO LLIBRERIA HASHLIB
+
+arxiu_texte=open("names.txt","r")  # Nom de l' arxiu i mode de obertura , en aquest cas LECTURA (READ)
+
+lineas = arxiu_texte.readlines()    #  LLEGIR LINEAS ARXIU 
+
+h = hashlib.new('md5')    # Es  crea  objecte de clase hash amb HASH MD5, SHA1, SHA256, SHA512, SH3-512
+j = hashlib.new('sha1')
+k = hashlib.new('sha256')
+l = hashlib.new('sha512')
+m = hashlib.new('sha3-512')
+for linea in lineas:
+  i=linea
+  linea = str.encode(linea)  # Es converteix el string en un byte string
+  print("El resultat en hexadecimal es del hash  MD5: "+ i +" es: " + str(h.hexdigest()))   # IMPRESSIÓ HASH en HEXADECIMAL
+  print("El resultat en hexadecimal es del hash  SHA-1: "+ i +" es: " + str(j.hexdigest()))
+  print("El resultat en hexadecimal es del hash  SHA-256: "+ i +" es: " + str(k.hexdigest()))
+  print("El resultat en hexadecimal es del hash  SHA-512: "+ i +" es: " + str(l.hexdigest()))
+  print("El resultat en hexadecimal es del hash  SHA3-512: "+ i +" es: " + str(m.hexdigest()))
+  h.update(linea)               # S'  agregan les  linea de l' arxiu  a l' objecte hash  
+  j.update(linea)
+  k.update(linea)
+  l.update(linea)
+  m.update(linea)
+
+arxiu_texte.close() # Tanco arxiu després de manipulació
+
+
+
+######################################################################################################
+
+#import hashlib    # Se importa hashlib
+
+#f = open('archivo.txt')    # Se abre el archivo con la información
+#lineas = f.readlines()    # Se leen las lineas del archivo
+
+#h = hashlib.new('md5')    # Se crea el objeto de clase hash
+#for linea in lineas:
+#  linea = str.encode(linea)  # Se convierte el string en un byte string
+#  h.update(linea)    # Se agregan las lineas del archivo al objeto hash
+
+#print("El resultado en bytes del hash es: " + str(h.digest())) # Se imprime el hash en bytes
+#print("El resultado en hexadecimales del hash es: " + str(h.hexdigest())) # Se imprime el hash en caracteres hexadecimales
---
+++ dockerfile
@@ -0,0 +1,49 @@
+FROM python:3.8.5-alpine3.11
+MAINTAINER  Andrés 
+WORKDIR /
+COPY . /
+ENTRYPOINT [ "python3" ]
+#CMD [ "SCRIPT_TXT_HASH.py" ]
+
+
+
+
+###################################
+
+#FROM python:3.8.5-alpine3.11
+#WORKDIR /usuari/python
+#COPY . .
+#ADD SCRIPT_TXT_HASH.py .
+#CMD ["SCRIPT_TXT_HASH.py"]
+#ENTRYPOINT ["python3"]
+
+
+
+##################################
+#FROM python:3.8.5-alpine3.11
+#WORKDIR /usuari/python
+#COPY . .
+#CMD ["SCRIPT_TXT_HASH.py"]
+#ENTRYPOINT ["python3"]
+
+##################################
+
+#FROM python:3.8.5-alpine3.11
+#WORKDIR /usr/src/app
+##COPY . . /usuari/python
+#COPY . .
+##VOLUME /usuari/python
+#CMD ["SCRIPT_TXT_HASH.py"]
+#ENTRYPOINT ["python3"]
+
+
+#####################################
+
+#FROM python:3.8.5-alpine3.11
+#FROM python:latest
+#COPY . /usr/src/app
+#WORKDIR /usr/src/app
+#ADD main.py / names.txt /
+#ENTRYPOINT python main.py
+#CMD [ "python", "./main.py" ]
+
