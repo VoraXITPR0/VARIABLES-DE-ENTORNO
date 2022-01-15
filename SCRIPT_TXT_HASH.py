86ced0401af7c5d1f5eb1e5ebed75a1a1a6c05dd
VoraXITPR0 <a2107507@institutmontilivi.cat>
2022-01-15 18:04:13 +0000

    SCRIPT OK VARIABLE ENTORN VE EN LOCAL

--- SCRIPT_TXT_HASH.py
+++ SCRIPT_TXT_HASH.py
@@ -1,10 +1,10 @@
-########  SCRIPT LLEGIR ARXIU TXT i crear HASH MD5,SHA1,SHA256,SHA512 iSHA3-512 By Andrés Manso  #############################
+########  SCRIPT LLEGIR ARXIU TXT AMB VARIABLES ENTORN VE i crear HASH MD5,SHA1,SHA256,SHA512 iSHA3-512 By Andrés Manso  #############################
 
 from io import open  # IMPORTO modul IO i metode "OPEN" ( permet obrir un arxiu extern)
 import hashlib       # IMPORTO LLIBRERIA HASHLIB
 import os       # IMPORTO  LLIBRERIA 
 
-os.environ['VE']
+os.environ['VE']="names.txt"
 arxiu_texte=open(os.environ.get('VE'))
 
 #arxiu_texte=open("/data/names.txt","r")  # Nom de l' arxiu i mode de obertura , en aquest cas LECTURA (READ)
